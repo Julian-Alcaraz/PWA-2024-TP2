eb339c118458fcd863cc814bbd6a78b97bbd53ba
Julian-Alcaraz <62039908+Julian-Alcaraz@users.noreply.github.com>
2024-05-06 19:37:46 -0300

    agrrego un poco de estilos

--- src/components/DownloadButton/DownloadButton.jsx
+++ src/components/DownloadButton/DownloadButton.jsx
@@ -6,20 +6,58 @@ const DownloadButton = () => {
         const response = await fetch("/mocks/vehiculos.json");
         const vehiculos = await response.json();
         const doc = new jsPDF();
-
-        doc.text("Listado de vehículos", 10, 10); // Título del listado
-
-        let y = 20; // Inicia
-        vehiculos.forEach((vehiculo) => {
+    
+        // Estilo para el título del listado
+        doc.setFontSize(18);
+        doc.setTextColor(100);
+        doc.text("Listado de vehículos", 10, 10);
+    
+        // Variables para la posición de la tabla
+        let startX = 10;
+        let startY = 20;
+        let rowHeight = 10;
+        let idWidth = 10;
+        let colWidth = 30; // Modifiqué el ancho de las otras columnas
+    
+        // Estilo para la tabla
+        doc.setDrawColor(0);
+        doc.setFontSize(10); // Tamaño de fuente para los datos de la tabla
+        doc.setTextColor(0); 
+        doc.setFillColor(200, 200, 200)
+        doc.rect(startX, startY, idWidth, rowHeight,'S');
+        doc.text("Id", startX + 2, startY + 7);
+        doc.rect(startX + idWidth, startY, colWidth, rowHeight,'S');
+        doc.text("Marca", startX + idWidth + 2, startY + 7);
+        doc.rect(startX + idWidth + colWidth, startY, colWidth, rowHeight,'S');
+        doc.text("Modelo", startX + idWidth + colWidth + 2, startY + 7);
+        doc.rect(startX + idWidth + 2 * colWidth, startY, colWidth, rowHeight,'S');
+        doc.text("Año", startX + idWidth + 2 * colWidth + 2, startY + 7);
+        doc.rect(startX + idWidth + 3 * colWidth, startY, colWidth, rowHeight,'S');
+        doc.text("Estado", startX + idWidth + 3 * colWidth + 2, startY + 7);
+        doc.rect(startX + idWidth + 4 * colWidth, startY, colWidth, rowHeight,'S');
+        doc.text("Precio", startX + idWidth + 4 * colWidth + 2, startY + 7);
+        vehiculos.forEach((vehiculo, index) => {
             const { id, marca, modelo, anio, estado, precio } = vehiculo;
-            const text = `ID: ${id}, Marca: ${marca}, Modelo: ${modelo}, Año: ${anio}, Estado: ${estado}, Precio: ${precio}`;
-            doc.text(text, 10, y);
-            y += 10; // Incrementar la posición Y para el siguiente vehículo
+    
+            // Establecer colores alternados para las filas
+            // Dibujar celdas
+            startY += rowHeight;
+            doc.rect(startX, startY, idWidth, rowHeight);
+            doc.text(id.toString(), startX + 2, startY + 7);
+            doc.rect(startX + idWidth, startY, colWidth, rowHeight);
+            doc.text(marca, startX + idWidth + 2, startY + 7);
+            doc.rect(startX + idWidth + colWidth, startY, colWidth, rowHeight);
+            doc.text(modelo, startX + idWidth + colWidth + 2, startY + 7);
+            doc.rect(startX + idWidth + 2 * colWidth, startY, colWidth, rowHeight);
+            doc.text(anio.toString(), startX + idWidth + 2 * colWidth + 2, startY + 7);
+            doc.rect(startX + idWidth + 3 * colWidth, startY, colWidth, rowHeight);
+            doc.text(estado, startX + idWidth + 3 * colWidth + 2, startY + 7);
+            doc.rect(startX + idWidth + 4 * colWidth, startY, colWidth, rowHeight);
+            doc.text(precio.toString(), startX + idWidth + 4 * colWidth + 2, startY + 7);
         });
-
+        // Guardar el PDF con el nombre especificado
         doc.save("listado_vehiculos.pdf");
-        doc.text("Listado de vehiculos!", 10, 10);
-        doc.save("a4.pdf");
+   
     }
   
     return (
